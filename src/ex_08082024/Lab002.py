03a74a27de436856674f95eb436ae979ac4cbbd6
swatimodi19 <swatimodi1019@gmail.com>
2024-08-16 14:37:57 +0530

    2nd class lab2

---
+++ src/ex_08082024/Lab002.py
@@ -0,0 +1,6 @@
+## print()
+print("Hello","Today is my 2nd class")
+print("Hello","Today is my 2nd class" , sep = ' | ' , end = '__')
+print("Hello","Today is my 2nd class" , sep = ' **** ' , end = '\n')
+print("Hello","Today is my 2nd class" , sep = ' ; ' , end = '_')
+
