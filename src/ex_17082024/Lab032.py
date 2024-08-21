ce5effb02a1a3c21349ef0a1c10b31bb76c2b276
swatimodi19 <swatimodi1019@gmail.com>
2024-08-22 00:36:23 +0530

    day 4

---
+++ src/ex_17082024/Lab032.py
@@ -0,0 +1,8 @@
+x=10
+y=20
+a= True
+print (x !=y)
+print ( not a )
+##   ! ==> not -> while using integers
+
+## not --> bool
---
+++ src/ex_17082024/Lab033.py
@@ -0,0 +1,11 @@
+##Ternary Operators
+
+# if condition then  <> else  do that
+my_age =18
+print ("i will go to goa" if my_age >18 else  "dont go")
+
+my_age = int(input("enter the age"))
+if  my_age > 18:
+    print ("go goa")
+else:
+    print ("dont go")
