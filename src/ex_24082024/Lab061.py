a4be67be7875e3023b163dcfed10c98bff025ff5
swatimodi19 <swatimodi1019@gmail.com>
2024-08-26 17:09:47 +0530

    practised 8th class i.e 24th Aug class and tasks

---
+++ src/ex_24082024/Lab061.py
@@ -0,0 +1,18 @@
+# ### Functions
+# ######Build in functions
+# input (); range() ;print() ;  type () ; id() ; pow() ; list(); len(); str;bool;float;str.lower() ; str.upper:
+# # block of code which can be reused
+#################3
+
+#######
+# User define functions
+# function has 2 parts
+#     Definition
+#     Def <name of a function>():
+def greet():
+    print("Hello world!!")
+#     calling
+greet()
+greet()
+greet()
+# Function may or may not return
--- src/ex_24082024/Lab062.py
+++ src/ex_24082024/Lab062.py
@@ -1,2 +1,24 @@
 ###Functions
-## Types of function is python
+
+## rule to write function name
+
+# --> def 123(): --> not correct
+# --> def _(): ---> correct
+# --> def test123(): ---> correct
+# --> def test_name_test(): --> correct this is a snake case or underscorew case
+# --> def testName(): --> camel case not allowed in python and function names should be in small case
+# --> def h(): --->
+# --> print("hi")     ---> indentation issue
+# --> def h():
+# -->   print("hi")     ---> correct indentation
+
+
+def h():
+    print("i m part of function")
+print("i m not a part of function due to not correct indentation, click on tab to make me part of function")
+
+
+
+## different Types of function is python
+#1. build-in functions :
+#2. User defined functions
