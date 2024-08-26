806b45dc1699797debacdd144085f4b17fbc6ea7
swatimodi19 <swatimodi1019@gmail.com>
2024-08-26 23:59:17 +0530

    practised 8th class i.e 24th Aug class and all the tasks are completed

--- src/Tasks List -Programs so far/Task11- Fibonaci Series.py
+++ src/Tasks List -Programs so far/Task11- Fibonaci Series.py
@@ -1,1 +1,22 @@
 # ✅ Fibonaci series 0,0+1, 0+1+1, n = 7 0, 1, 2, 3, 5, 8, 13
+
+
+ 0|1||
+
+num= int(input("enter the number \n"))
+fib0 = 0
+fib1 = 1
+fib = fib0+fib1
+
+if num==0 or num == 1:
+    fib = num
+    print(fib)
+
+else:
+    for i in range(1,num+1):
+        fib = fib+(i-1)
+
+1 = 1+0
+2 =1+1
+ =1+2
+
