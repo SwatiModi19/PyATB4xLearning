18aa096dcb601f3a016891632504b3fedcc397cc
swatimodi19 <swatimodi1019@gmail.com>
2024-09-01 14:35:42 +0530

    practised 8th class i.e 24th Aug class and all the tasks are completed

--- src/Tasks List -Programs so far/Task11- Fibonaci Series.py
+++ src/Tasks List -Programs so far/Task11- Fibonaci Series.py
@@ -1,22 +1,16 @@
 # ✅ Fibonaci series 0,0+1, 0+1+1, n = 7 0, 1, 2, 3, 5, 8, 13
-
-
- 0|1||
-
-num= int(input("enter the number \n"))
-fib0 = 0
-fib1 = 1
-fib = fib0+fib1
-
-if num==0 or num == 1:
-    fib = num
-    print(fib)
-
-else:
-    for i in range(1,num+1):
-        fib = fib+(i-1)
-
-1 = 1+0
-2 =1+1
- =1+2
-
+# from telnetlib import VT3270REGIME
+#
+# 0 + 1, 1 + 1, 2 + 1, 3 + 2, 5 + 3
+####
+p1 = 1
+c1 = 1
+R1 = 0
+fact = list[0,1]
+num = int (input("enter the number \n"))
+for i in range(2, num + 1):   # 3 = 2+1
+    R1 = p1+ c1     # 1+1= 2  ; 1 +2 =3 ; 5 =
+    p1= c1
+    c1=R1           # p1 = c1 : 2
+                # c1 = R1 :
+    print (f" {R1}" , end= ", ")
