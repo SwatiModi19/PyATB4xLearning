966705935aae6eb9b0edb8897dc9a7ed0020b167
swatimodi19 <swatimodi1019@gmail.com>
2024-09-03 20:28:28 +0530

    practised 31st Aug class and all the tasks are completed

--- src/ex_31082024/Lab_109.py
+++ src/ex_31082024/Lab_109.py
@@ -3,9 +3,8 @@
 ### It will automaticaaly be called when an object is created
 
 
-class dog():
-
-    #A
+class dog:
+    # A
     name = None
 
     # constructor  has no return type
@@ -16,19 +15,19 @@ class dog():
     #     print("Called , Object is created")
     #     self.name = name
 
-    def __init__(self, name ,age):
+    def __init__(self, name, age):
         print("Called , Object is created")
         self.name = name
         self.age = age
 
-    #B
+    # B
     def sleep(self):
         print("i can sleep")
-        print ("who is sleeping "  + self.name , self.age)
+        print("who is sleeping " + self.name, self.age)
 
 
 # Dog1 = dog()   ### On running --> __init__ will automaticaaly be called  -> this is example of default constructor with no arguments
-#print(Dog1.name) ## None
+# print(Dog1.name) ## None
 
 # Dog2 = dog("chow")   ### On running --> __init__ will automaticaaly be called with arguments
 # print(Dog2.name)
@@ -36,8 +35,7 @@ class dog():
 # Dog3 = dog("Mow")
 # print(Dog3.name)
 
-Dog4 = dog("Mow" , 10)
+Dog4 = dog("Mow", 10)
 print(Dog4.name)
 print(Dog4.age)
 Dog4.sleep()
-
